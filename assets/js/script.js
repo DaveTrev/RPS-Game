0000b20f610e4583272773dfdbd04da673426d60
DaveTrev <trevaskisdavid@gmail.com>
2023-03-09 21:48:49 +0000

    modal working start js script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,6 @@
 let userScore = 0
 let computerScore = 0
 
-const userScore_td = document.getElementById("user-score")
-const computerScore_td = document.getElementById("computer-score")
-const status = document.getElementById("status")
-const rock_div = document.getElementById("rock")
-const paper_div = document.getElementById("paper")
-const scissors_div = document.getElementById("scissors")
 
 
 // modal open and close
@@ -24,14 +18,3 @@ close.addEventListener('click', () => {
 });
 
 
-function getComputerChoice() {
-    const game_choices = [rock, paper, scissors];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return game_choices[randomNumber];
-};
-
-function main() {
-    rock_div.addEventListener('click', () => game("rock"));
-    paper_div.addEventListener('click', () => game("paper"));
-    scissors_div.addEventListener('click', ()=>game("scissors"));
-}
